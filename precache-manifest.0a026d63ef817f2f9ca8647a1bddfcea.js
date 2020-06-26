aa7e8b7332b1a495ed5118a67cf39fc98b57761d
Ada Cheung <ada.cheung96@gmail.com>
2020-06-26 15:43:23 -0400

    Updates

---
+++ precache-manifest.0a026d63ef817f2f9ca8647a1bddfcea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacedf737f883b2673d44d1308c51f5a",
+    "url": "/PostNotes/index.html"
+  },
+  {
+    "revision": "7e2ec132e1a777b9ce62",
+    "url": "/PostNotes/static/js/2.0c5e0a05.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/PostNotes/static/js/2.0c5e0a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200a74dd0c71f69359a1",
+    "url": "/PostNotes/static/js/main.ee1ae230.chunk.js"
+  },
+  {
+    "revision": "b1fc0f491eddb563b780",
+    "url": "/PostNotes/static/js/runtime-main.5feeb322.js"
+  }
+]);
